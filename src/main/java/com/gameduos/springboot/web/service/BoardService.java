528fab7696e90d12d214701211346efb59d35a22
matamong <matamatamong@gmail.com>
2020-02-01 16:48:08 +0900

    Apply board sort

--- src/main/java/com/gameduos/springboot/web/service/BoardService.java
+++ src/main/java/com/gameduos/springboot/web/service/BoardService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 
@@ -16,8 +17,8 @@ public class BoardService {
     private final BoardRepository boardRepository;
 
     public Page<Board> findBoardList(Pageable pageable) {
-        pageable = PageRequest.of(pageable.getPageNumber() <= 0 ? 0 : pageable.
-                getPageNumber() - 1, pageable.getPageSize());
+        int page = (pageable.getPageNumber() == 0) ? 0 : (pageable.getPageNumber() - 1);
+        pageable = PageRequest.of(page, 10, new Sort(Sort.Direction.DESC, "idx")); // <- Sort 추가
         return boardRepository.findAll(pageable);
     }
 

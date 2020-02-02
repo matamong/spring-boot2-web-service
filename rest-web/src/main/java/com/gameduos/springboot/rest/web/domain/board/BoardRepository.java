2e2257ed8720d70cfd736e1f30a76fd5f959f0d9
matamong <matamatamong@gmail.com>
2020-02-02 16:59:29 +0900

    Delete unnessary module

--- rest-web/src/main/java/com/gameduos/springboot/rest/web/domain/board/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gameduos.springboot.rest.web.domain.board;
-
-import com.gameduos.springboot.rest.web.domain.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-    Board findByUser(User user);
-}

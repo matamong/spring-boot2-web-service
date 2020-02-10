d57c1a2e32ba17a087b8f704efc3e14703ef1a96
matamong <matamatamong@gmail.com>
2020-02-10 19:51:35 +0900

    포인트 서비스 수정 - point 자체가 null일 경우 대비 - 오타 수정 - 지급 포인트 수 변경

--- src/main/java/com/gameduos/springboot/web/service/PointService.java
+++ src/main/java/com/gameduos/springboot/web/service/PointService.java
@@ -5,21 +5,21 @@ import com.gameduos.springboot.web.domain.point.Point;
 import com.gameduos.springboot.web.domain.point.PointRepository;
 import com.gameduos.springboot.web.domain.point.PointType;
 import com.gameduos.springboot.web.domain.user.User;
-import com.gameduos.springboot.web.dto.BoardSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+
 @RequiredArgsConstructor
 @Service
 public class PointService {
     private final PointRepository pointRepository;
 
-    private int boardPoint = 10;
-    private int loginPoint = 1;
-    private int referralCodePoint = -50;
+    private int boardPoint = 100;
+    private int loginPoint = 100;
+    private int referralCodePoint = 50;
 
     public void boardPointSave (User user) {
         pointRepository.save(Point.builder()
@@ -39,28 +39,24 @@ public class PointService {
                 .build());
     }
 
-    public void ReferralPointCut (@SocialUser User user) {
+    public void referralPointCut (@SocialUser User user) {
         pointRepository.save(Point.builder()
                 .user(user)
-                .point(referralCodePoint)
+                .point(-referralCodePoint)
                 .pointType(PointType.CODE_CREATE)
                 .pointGiveDate(LocalDateTime.now())
                 .build());
     }
 
     public boolean measureReferralPoint (User user) {
-        boolean isPointEnough = false;
-        int totalPoint = 0;
-
-        List<Point> pointList = pointRepository.findByUser(user);
-
-        for(Point point : pointList) {
-            totalPoint += point.getPoint();
+        if(isPointNotNull(user)){
+            System.out.println(user.getEmail() +"의 PointService null ==================== " + false);
+            int totalPoint = sumPoint(user);
+            System.out.println(user.getEmail() +"의 PointService point sum ==================== " + totalPoint);
+            return totalPointMeasure(totalPoint);
         }
-        if(totalPoint >= referralCodePoint) {
-            isPointEnough = true;
-        }
-        return isPointEnough;
+        System.out.println(user.getEmail() +"의 PointService null ==================== " + true);
+        return false;
     }
 
     public boolean measurePoint (int pointToSubtract, User user) {
@@ -76,5 +72,34 @@ public class PointService {
             isPointEnough = true;
         }
         return isPointEnough;
+    }
+
+    public boolean isPointNotNull(User user){
+        List<Point> pointList = pointRepository.findByUser(user);
+
+        if(user != null && pointList.isEmpty() == false) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public int sumPoint(User user){
+        int totalPoint = 0;
+
+        List<Point> pointList = pointRepository.findByUser(user);
+
+        for(Point point : pointList) {
+            totalPoint += point.getPoint();
+        }
+        return totalPoint;
+    }
+
+    public boolean totalPointMeasure(int totalPoint){
+        if(totalPoint >= referralCodePoint) {
+            return true;
+        }else{
+            return false;
+        }
     }
 }

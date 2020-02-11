2eaf4fda5267a9cd481b9f8fb46a41474029952f
matamong <matamatamong@gmail.com>
2020-02-11 21:21:34 +0900

    MyPageController : 메소드 이름 변경 (referral -> referralCode)

--- src/main/java/com/gameduos/springboot/web/controller/MyPageController.java
+++ src/main/java/com/gameduos/springboot/web/controller/MyPageController.java
@@ -2,29 +2,31 @@ package com.gameduos.springboot.web.controller;
 
 import com.gameduos.springboot.web.annotation.SocialUser;
 import com.gameduos.springboot.web.domain.user.User;
+import com.gameduos.springboot.web.service.BoardService;
+import com.gameduos.springboot.web.service.ReferralCodeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequiredArgsConstructor
 @Controller
+@RequestMapping("/myPage")
 public class MyPageController {
-    @GetMapping("/myPage")
-    public String myPage(Model model, @SocialUser User user) {
 
-        if (user != null) {
-            System.out.println(user.getEmail());
-            model.addAttribute("userEmail", user.getEmail());
-        }
+    private final ReferralCodeService referralCodeService;
+
+    @GetMapping({"", "/"})
+    public String myPage(@SocialUser User user) {
         return "myPage/myPage";
     }
 
-    @GetMapping("/myPage/referralCode")
-    public String referralCode(Model model, @SocialUser User user) {
-
-        if (user != null) {
-            System.out.println(user.getEmail());
-            model.addAttribute("userEmail", user.getEmail());
-        }
+    @GetMapping("/referralCode")
+    public String referralCode(@PageableDefault Pageable pageable, Model model, @SocialUser User user) {
+        model.addAttribute("referralCodeList", referralCodeService.findBoardList(pageable));
         return "myPage/referralCode";
     }
 }

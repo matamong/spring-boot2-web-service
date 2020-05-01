ec5c6e080a7f3890ec055411c90ca8e5dfb1f5d6
matamong <matamatamong@gmail.com>
2020-05-01 21:41:44 +0900

    필요없는 코드 삭제

--- src/main/java/com/gameduos/springboot/web/controller/BoardRestController.java
+++ src/main/java/com/gameduos/springboot/web/controller/BoardRestController.java
@@ -1,28 +1,17 @@
 package com.gameduos.springboot.web.controller;
 
 import com.gameduos.springboot.web.annotation.SocialUser;
-import com.gameduos.springboot.web.domain.board.Board;
-import com.gameduos.springboot.web.domain.board.BoardRepository;
+
 import com.gameduos.springboot.web.domain.user.User;
 import com.gameduos.springboot.web.dto.BoardSaveRequestDto;
 import com.gameduos.springboot.web.dto.BoardUpdateRequestDto;
 import com.gameduos.springboot.web.service.BoardService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.hateoas.PagedResources;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 
 @RequiredArgsConstructor

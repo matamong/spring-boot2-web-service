f9aeff72bb76295d869021ca48b8b43e71d5b322
matamong <matamatamong@gmail.com>
2020-05-13 15:58:06 +0900

    heroMatchingResult.html : 히어로 심리테스트 결과 페이지 추가

---
+++ src/main/resources/static/js/PersonalTest/ow/heroMatchinResult.js
@@ -0,0 +1,50 @@
+//이미지주소만 어떻게 하자
+
+
+// 화면에 쓰는 js
+var heroEngName = $('#hero').val();
+var x = $('x').val();
+var y = $('y').val();
+
+var heroArr = findHeroArray(heroEngName)
+
+changeOgMeta(heroArr);
+writeArticle(heroArr);
+
+function findHeroArray(heroEngName) {
+    for (i in HEROS) {
+        if (HEROS[i][0] === heroEngName) {
+            return HEROS[i];
+        }
+    }
+}
+
+function changeOgMeta(heroArr) {
+    var heroEngName = heroArr[0];
+    var heroKorName = heroArr[3];
+
+    var title = '나는 ' + heroKorName + '와(과) 잘 어울려요.'
+    var imgSrc = 'https://raw.githubusercontent.com/gameduosdev/gameduosdev.github.io/master/' + heroEngName + '.png';
+    var url = $(location).attr('href');
+
+    // 페이스북 관련 메타태그
+    $("#ogTitle").attr("content", title);
+    $("#ogImg").attr("content", imgSrc);
+    $("#ogUrl").attr("content", url);
+
+    //hidden input에 주소 쓰기
+    $("#link").val(url);
+    //본문 이미지 바꾸기
+    $("#heroImg").attr("src", imgSrc);
+}
+
+function writeArticle(heroArr) {
+    var heroKorName = heroArr[3];
+    var heroQuote = heroArr[4];
+    var heroDescription = heroArr[5];
+
+    $('#heroName').text(heroKorName);
+    $('.heroName').text(heroKorName);
+    $('#blockquote').text(heroQuote);
+    $('#heroDescription').text(heroDescription);
+}

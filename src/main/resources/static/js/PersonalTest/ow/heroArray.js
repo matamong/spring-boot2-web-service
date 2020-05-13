e39db4dbfd12d5875fe66611ba1de9aed1b2ecb8
matamong <matamatamong@gmail.com>
2020-05-13 15:58:38 +0900

    heroArray.js : 히어로 어레이 따로 뺌

---
+++ src/main/resources/static/js/PersonalTest/ow/heroArray.js
@@ -0,0 +1,41 @@
+const WIDOWMAKER = ['WIDOWMAKER', 20, 0, "위도우메이커"];
+const ROADHOG = ['ROADHOG', 20, 4, '로드호그'];
+const REAPER = ['REAPER', 19, 1, '리퍼'];
+const SIGMA = ['SIGMA', 18, 2, '시그마'];
+const MOIRA = ['MOIRA', 17, 0, '모이라'];
+const SYMMETRA = ['SYMMETRA', 16, 0, '시메트라'];
+const HANZO = ['HANZO', 16, 2, '한조'];
+const ZENYATTA = ['ZENYATTA', 15, 20, '젠야타', '진정한 자아엔 형체가 없는 법'
+    , '젠야타는 인간과 기계 사이의 갈등을 가르침이 아닌 교감으로 해소하고자 하는 방랑자입니다.'
+    + '자신의 신념을 위해서라면 스승과 동료들을 뒤로 하고 혼자 활동하는 것도 마다하지 않는 성격을 가지고 있습니다.'
+    + '평화를 찾아 세계를 떠도는 젠야타는 무고한 자들을 위해 도움의 손길을 건내는걸 마다하지 않을 정도로 온화한 인성의 소유자이네요.'
+    + ' 어쩌면 이런 점이 당신과 젠야타의 공통점일지도 모르겠어요!'];
+const BASTION = ['BASTION', 14, 10, '바스티온'];
+const DOOMFIST = ['DOOMFIST', 13, 0, '둠피스트'];
+const SOLDIER76 = ['SOLDIER76', 12, 2, '솔저 76'];
+const MCREE = ['MCREE', 12, 12, '맥크리'];
+const SOMBRA = ['SOMBRA', 12, 4, '솜브라'];
+const ZARYA = ['ZARYA', 11, 6, '자리야'];
+const MEI = ['MEI', 10, 20, '메이'];
+const WREKINGBALL = ['WREKINGBALL', 10, 4, '레킹볼'];
+const PHARAH = ['PHARAH', 10, 2, '파라'];
+const TORBJORN = ['TORBJORN', 9, 2, '토르비욘'];
+const ASH = ['ASH', 9, 14, '애쉬'];
+const ANA = ['ANA', 8, 6, '아나'];
+const GENJI = ['GENJI', 8, 16, '겐지'];
+const MERCY = ['MERCY', 7, 8, '메르시'];
+const JUNKRAT = ['JUNKRAT', 6, 16, '정크랫'];
+const WINSTON = ['WINSTON', 6, 14, '윈스턴'];
+const ECHO = ['ECHO', 5, 8, '에코'];
+const ORISA = ['ORISA', 5, 6, '오리사'];
+const BRIGITTE = ['BRIGITTE', 4, 18, '브리기테'];
+const BAPTISTE = ['BAPTISTE', 3, 14, '바티스트'];
+const DVA = ['DVA', 2, 8, '디바'];
+const REINHARDT = ['REINHARDT', 1, 18, '라인하르트'];
+const TRACER = ['TRACER', 0, 16, '트레이서'];
+const LUCIO = ['LUCIO', 0, 20, '루시우'];
+
+const HEROS = [];
+HEROS.push(WIDOWMAKER, ROADHOG, REAPER, SIGMA, MOIRA, SYMMETRA, HANZO, ZENYATTA, BASTION, DOOMFIST, SOLDIER76, MCREE,
+    SOMBRA, ZARYA, MEI, WREKINGBALL, PHARAH, TORBJORN, ASH, ANA, GENJI,
+    MERCY, JUNKRAT, WINSTON, ECHO, ORISA, BRIGITTE, BAPTISTE, DVA, REINHARDT, TRACER, LUCIO);

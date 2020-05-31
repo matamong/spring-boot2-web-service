e029e330ff38c0f2919bfe3fc0732e9b1bedcce7
matamong <matamatamong@gmail.com>
2020-05-31 22:40:34 +0900

    basic.js : og meta 자동으로 변경해주는 js 추가

--- src/main/resources/static/js/basic.js
+++ src/main/resources/static/js/basic.js
@@ -1,3 +1,5 @@
+
+
 var token = $("meta[name='_csrf']").attr("content");
 var header = $("meta[name='_csrf_header']").attr("content");
 $(function() {
@@ -9,3 +11,15 @@ $(function() {
 const protocol = location.protocol;
 const host = location.host;
 const url = protocol + "//" + host+'/';
+
+
+function changeMeta() {
+
+    var thisUrl = $(location).attr('href');
+    var title = $(document).find("title").text();
+
+    $("#ogTitle").attr("content", title);
+    $("#ogUrl").attr("content", thisUrl);
+}
+
+changeMeta();

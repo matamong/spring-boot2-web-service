5dafd283d8e19e978e10e81381983575e2e07ba8
matamong <matamatamong@gmail.com>
2020-06-03 19:58:00 +0900

    <etc> 적립,소멸 포인트 변경 / 포인트 적립, 소멸 안내 메시지 추가 - 게시글 10 , 댓글 1, 추천코드 50, 개인정보수정 1000

--- src/main/java/com/gameduos/springboot/web/service/ReferralCodeService.java
+++ src/main/java/com/gameduos/springboot/web/service/ReferralCodeService.java
@@ -12,16 +12,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 

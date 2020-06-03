5f78ff97709db0de22de2c7ebc735d0fc4cd9e8e
matamong <matamatamong@gmail.com>
2020-06-03 19:06:56 +0900

    <etc> : 구글 애널리틱스 js 적용

--- src/main/resources/static/js/basic.js
+++ src/main/resources/static/js/basic.js
@@ -25,3 +25,9 @@ function changeMeta() {
 
 changeMeta();
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-168455544-1');
